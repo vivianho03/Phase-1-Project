7340386e1563fd767691a322e2a055dabe294190
Vivian Ho <ho.vivian89@gmail.com>
2023-01-10 22:24:19 -0800

    basic-search complete

--- index.js
+++ index.js
@@ -4,50 +4,57 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 // event listeners
+document.addEventListener('DOMContentLoaded', basicSearch);
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
-//get eventListener to mouse Over to display modal
 mealList.addEventListener("mouseover", (event) => {
     // highlight the mouseover target
-    event.target.style.color = "orange";
+    event.target.style.color = "white";
   
     // reset the color after a short delay
     setTimeout(() => {
       event.target.style.color = "";
     }, 500);
   }, false);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
 
-//Add a function to your code base that makes a search in the mealDB api for tomato.
+// Add a function to your code base that makes a search in the mealDB api for tomato.
 function basicSearch() {
-    
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=tomato`)
+    let searchInputTxt = document.getElementById('search-input').value = "tomato";
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
-        let basicDiv = document.getElementById("basic-search");
-        let basicHeader = document.createElement("h2");
-        basicHeader.innerHTML = "Current Search: Tomato"
-        basicDiv.append(basicHeader);
+    let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
         
-    })
+    
 }
-//create function basicSearch
-//fetch API that grabs Tomato
-//then write the two .then 
 
-//Process the results and put them on the page.
-
-//Add a div with h2 that says current search tomato.
-
-//create variable for div with id="basic-search" 
-//create a variable for h2 and createElemeent
-//have this variable have a innerHTML "current search: tomato"
-//append h2 to div with id="basic-search"
-
-//When a new search is sent out we will change the word after current search to reflect the new search and the page works as before.
-
-
-
-// get meal list that matches with the ingredients
+// get meal list that matches with the ingredients inputted within search
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
